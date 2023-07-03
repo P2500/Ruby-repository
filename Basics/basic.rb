b7bfc0713dcfce0fd78aa9e8b63d22ae4f66d996
paulohdb <paulinhogt6@gmail.com>
2023-07-03 11:59:39 -0400

    Coding the basic

---
+++ Basics/basic.rb
@@ -0,0 +1,41 @@
+# This is a comment line
+
+=begin
+  This is a multi-line comments,
+	Tabs used 2 here.
+=end
+
+# Variables
+
+myString = "first string!"
+myBoolean = true
+myNumber = 3
+
+# Some uses with String methods
+
+myString.length
+myString.reverse
+myString.upcase
+myString.downcase.reverse
+
+# Printing variables and strings
+
+number = 3
+
+puts "I have #{number} rubik's cube in my front!"
+
+# Arrays
+
+array = []
+array2 = [1, 5, 6, 7]
+
+array2[2] = 2
+
+# Hashes and Symbols
+
+myHash = {  # Creating a Hash
+	"Key" => "value",
+	"Key2" => "value2"
+}
+
+puts myHash["Key"] # puts value
