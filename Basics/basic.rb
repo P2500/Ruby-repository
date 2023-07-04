e7d55e37bb2143837a7fc19f29982a708fd7dfee
paulohdb <paulinhogt6@gmail.com>
2023-07-04 12:44:25 -0400

    learning conditional

--- Basics/basic.rb
+++ Basics/basic.rb
@@ -5,7 +5,7 @@
 	Tabs used 2 here.
 =end
 
-# Variables
+#  ======== Variables ========
 
 myString = "first string!"
 myBoolean = true
@@ -24,14 +24,14 @@ number = 3
 
 puts "I have #{number} rubik's cube in my front!"
 
-# Arrays
+#  ======== Arrays ========
 
 array = []
 array2 = [1, 5, 6, 7]
 
 array2[2] = 2
 
-# Hashes and Symbols
+#  ======== Hashes and Symbols ========
 
 myHash = {  # Creating a Hash
 	"Key" => "value",
@@ -39,3 +39,34 @@ myHash = {  # Creating a Hash
 }
 
 puts myHash["Key"] # puts value
+
+# Other way to create a hash using symbol
+myHash = Hash.new()
+myHash[:Key] = "value"
+myHash[:Key2] = "value2"
+
+
+# ======== Conditional statements ======== 
+
+if 3 > 2
+	puts 5
+end
+
+# Example
+
+number = 30
+num = 10
+booleanOne = true
+
+
+if booleanOne
+	if number < num
+		puts "number #{num} is bigger than #{number}" 
+	
+	elsif number > num
+		puts "number #{number} is bigger than #{num}"
+	
+	else
+		puts "i don't know numbers"
+	end
+end 
